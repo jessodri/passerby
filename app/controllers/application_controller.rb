3b921881cfdf61d67a26b7369c1cf4aa4be55b3a
Jessica <30883219+jessodri@users.noreply.github.com>
2018-05-03 11:59:39 +1000

    Add tables authentication to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :authenticate_user!, except: [:home]
+  before_action :authenticate_user!, except: [:home, :table, :all_tables]
 
 end
