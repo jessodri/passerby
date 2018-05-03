7ed68ef12ec1eda60ae70347d244b20f706f4ba4
Jessica <30883219+jessodri@users.noreply.github.com>
2018-05-03 10:58:39 +1000

    Add profile model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180502234334) do
+ActiveRecord::Schema.define(version: 20180503005400) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "profiles", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.text "bio"
+    t.text "image_data"
+    t.string "address_line_one"
+    t.string "address_line_two"
+    t.string "city"
+    t.string "state"
+    t.string "postcode"
+    t.string "country_code"
+    t.string "latitude"
+    t.string "longitude"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -32,4 +51,5 @@ ActiveRecord::Schema.define(version: 20180502234334) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "profiles", "users"
 end
