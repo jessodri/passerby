ef0742442653235590d2f56d1e326784163529bf
Jessica <30883219+jessodri@users.noreply.github.com>
2018-05-06 20:32:25 +1000

    Add fix

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,17 @@
 require_relative 'boot'
 
 require 'rails/all'
-require 'dotenv'
+require "rails"
+# Pick the frameworks you want:
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "action_cable/engine"
+require "sprockets/railtie"
+# require "rails/test_unit/railtie"
 require "image_processing"
 
 # Require the gems listed in Gemfile, including any gems
