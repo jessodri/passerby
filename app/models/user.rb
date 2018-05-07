de13b6afe38c94861a59e3142cc36157ba26a281
Jessica <30883219+jessodri@users.noreply.github.com>
2018-05-07 14:56:31 +1000

    small change

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  # after_create :create_profile
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_one :profile
@@ -6,5 +8,13 @@ class User < ApplicationRecord
   has_many :conversations
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+ 
 
+  # def create_profile
+  #   Profile.create!([
+  #     {
+  #     user_id: self.id
+  #     }
+  #   ])
+  # end
 end
