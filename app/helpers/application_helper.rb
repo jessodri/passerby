fe5e905e69dfaaa6dcf3b8400a305315313c0db2
Jessica <30883219+jessodri@users.noreply.github.com>
2018-05-07 15:59:49 +1000

    Add conversations and messages controllers/views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,6 @@ module ApplicationHelper
     # def google_map_image_tag_partial(pickup)
     #     image_tag "http://maps.googleapis.com/maps/api/staticmap?center=#{@pickup_request.profile.latitude},#{@pickup_request.profile.longitude}&zoom=14&size=400x400&sensor=false"
     # end
+
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
 
+  get 'messages/index'
+
+  get 'conversations/index'
+
   root 'pages#home'
 
   devise_for :users
   resources :profiles
   resources :pickup_requests
+  resources :conversations, only: [:index, :create] do
+    resources :messages, only: [:index, :create]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
