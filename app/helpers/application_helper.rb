20b39cdc16fca32c62ffba6a09b9c40b322a0c1d
Jessica <30883219+jessodri@users.noreply.github.com>
2018-05-09 19:37:17 +1000

    Add css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
     def google_map_image_tag(profile)
-        image_tag "http://maps.googleapis.com/maps/api/staticmap?center=#{@profile.latitude},#{@profile.longitude}&zoom=14&size=400x400&sensor=false"
+        image_tag "http://maps.googleapis.com/maps/api/staticmap?center=#{@profile.latitude},#{@profile.longitude}&zoom=14&size=300x300&sensor=false"
     end
 
     # def google_map_image_tag_partial(pickup)
